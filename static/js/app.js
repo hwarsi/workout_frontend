6c51161492e856e7e0f0314709a26591dee3edb0
hwarsi <hwarsi@g.clemson.edu>
2020-01-25 14:37:27 -0500

    Start

---
+++ static/js/app.js
@@ -0,0 +1,55 @@
+// $("") Known as a selector
+​
+// This is what a function looks like
+function exampleFunc() {
+    console.log('HEY! RUN ME!')
+}
+​
+// This runs a function
+exampleFunc();
+​
+// This is an example of a click event for a button running a function
+$("#color_change").click(function() {
+    $("#OuterContainer").css({"background":"violet"});
+});
+​
+// Examples of variables
+var x = 12;
+var y = x*5;
+​
+// How to debug and figure out your code
+console.log(y);
+​
+// Example if statement
+if (x === 12) {
+    console.log('DO THIS');
+} else if (x === 15) {
+    console.log('DO THAT');
+}
+​
+// Example of a list/array
+var exampleList = [1,2,3,4,5]
+​
+// Example for loop
+for (var i=0; i < exampleList.length(); i++) {
+    console.log(exampleList[i]);
+}
+Collapse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
